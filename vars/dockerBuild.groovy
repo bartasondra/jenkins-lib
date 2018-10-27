abbf00e7dec29f235fb2eef2eff79689f66cc85b
bartasondra <32699136+bartasondra@users.noreply.github.com>
2018-10-27 20:35:17 +0200

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
 def call(Map project) {
 	stage("Docker build") {
-		steps {
+		
 			echo "building $project.registry/$project.name:$project.version"
 			//sh "docker build -t $project.registry/$project.name:$project.version -t $project.registry/$project.name:latest ."
-		}
+		
 	}
 }
