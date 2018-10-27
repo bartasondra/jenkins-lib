9ba6d5abdcbc48988b6114760394f5cb511ef2e8
bartasondra <32699136+bartasondra@users.noreply.github.com>
2018-10-27 20:33:04 +0200

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -8,7 +8,7 @@ def call(Map project) {
 				steps{
 				script {
 					echo "$project.registry/$project.name:$project.version"
-				//dockerBuild(name: $project.name, version: $project.version, registry: $project.registry)
+				dockerBuild(name: "$project.name", version: "$project.version", registry: "$project.registry")
 
 				//dockerPush(name: $project.name, version: $project.version, registry: $project.registry)
 				}
